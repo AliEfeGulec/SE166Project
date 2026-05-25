f789d3564190d106348a187809b3ff216ab8760d
utkudurmazz <utkudurmaz9@gmail.com>
2026-05-26 01:17:59 +0300

    Cell,ServiceType and UtilityTypes

---
+++ src/services/ServiceType.java
@@ -0,0 +1,9 @@
+package services;
+
+public interface ServiceType {
+    String security = "security";
+    String health = "health";
+    String education = "education";
+
+    String[] services = {security,health,education};
+}
---
+++ src/utilities/UtilityType.java
@@ -0,0 +1,9 @@
+package utilities;
+
+public interface UtilityType {
+    String electricity = "electricity";
+    String water = "water";
+    String internet = "internet";
+
+    String[] utilities = {electricity,water,internet};
+}
