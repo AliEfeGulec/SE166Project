a707f7d2f915f06d2469decf09bde6850877918f
AliEfeGulec <aliefegulec2006@gmail.com>
2026-05-31 18:32:48 +0300

    Fill methods in Commercial.java , Industrial.java

--- src/zones/Commercial.java
+++ src/zones/Commercial.java
@@ -1,5 +1,52 @@
 package zones;
 
-public class Commercial {
+import resources.ResourceType;
+import services.ServiceType;
+import utilities.UtilityType;
 
+import java.util.List;
+import java.util.Set;
+
+public class Commercial extends Zone {
+    public static final char Symbol = 'C';
+
+    public Commercial(int row, int col) {
+        super(row, col);
+    }
+
+    @Override
+    public char getSymbol() {
+        return Symbol;
+    }
+
+    @Override
+    public String displayName() {
+        return "Commercial";
+    }
+
+    @Override
+    public List<String> requiredUtilities() {
+        return List.of(UtilityType.electricity,UtilityType.internet,UtilityType.water);
+
+        /*List<String> utilities = new ArrayList<>();
+        utilities.add(UtilityType.electricity);
+        utilities.add(UtilityType.internet);
+        utilities.add(UtilityType.water);
+        return utilities; long version AEG*/
+    }
+
+    @Override
+    public Set<String> requiredServices() {
+        return Set.of(ServiceType.security);
+    }
+
+    @Override
+    public String producedResource() {
+        return ResourceType.lifestyle;
+    }
+
+    @Override
+    protected int level3Bonus() {
+        return Math.min(resource(ResourceType.population),resource(ResourceType.goods));
+    }
 }
--- src/zones/Industrial.java
+++ src/zones/Industrial.java
@@ -1,5 +1,54 @@
 package zones;
 
-public class Industrial {
+import resources.ResourceType;
+import services.ServiceType;
+import utilities.UtilityType;
+
+import java.util.List;
+import java.util.Set;
+
+public class Industrial extends Zone {
+    public static final char Symbol = 'I';
+
+    public Industrial(int row, int col) {
+        super(row, col);
+    }
+
+    @Override
+    public char getSymbol() {
+        return Symbol;
+    }
+
+    @Override
+    public String displayName() {
+        return "Industrial";
+    }
+
+    @Override
+    public List<String> requiredUtilities() {
+        return List.of(UtilityType.electricity,UtilityType.water);
+
+        /*List<String> utilities = new ArrayList<>();
+        utilities.add(UtilityType.electricity);
+        utilities.add(UtilityType.water);
+        return utilities; long version AEG*/
+    }
+
+    @Override
+    public Set<String> requiredServices() {
+        return Set.of(ServiceType.security);
+
+    }
+
+    @Override
+    public String producedResource() {
+        return ResourceType.goods;
+    }
+
+    @Override
+    protected int level3Bonus() {
+        return resource(ResourceType.population);
+    }
+
 
 }
